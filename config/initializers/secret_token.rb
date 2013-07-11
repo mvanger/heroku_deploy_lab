704f80f19c5aeae419f4c845e5ffdaa22f4b2e13
David Fisher <tibbon+github@gmail.com>
2013-07-11 13:16:11 -0400

    Ran rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuDeployLab::Application.config.secret_token = '1a9cf58f14c20c7b2395bf08dcafe76933a0a88aa5b84dcb0c37fb1f46fe454858ede28551e4c3c1d6fc3b4eb7798bc8deb3ac1031b012195cb1b0d273f03059'
